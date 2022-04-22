228a8acfde87940e7f894a30dd74b22d00f2910c
kapezit <gretmail@gmail.com>
2022-04-22 12:21:18 -0700

    Many changes
    Working April 22, 2022

--- rl_items_in_progress.py
+++ rl_items_in_progress.py
@@ -6,6 +6,7 @@ import json
 import os
 import mysql.connector
 from mysql.connector import errorcode
+from playwright.sync_api import sync_playwright
 
 
 ##################################
@@ -20,12 +21,34 @@ def base_url(iid, paintid):
 
 def get_page(url):
     """Action: Parse html content with bs4. Requires: url as the parameter."""
-    result = requests.get(url)
-    return BeautifulSoup(result.content, 'html.parser')
+    #result = requests.get(url, timeout=12)
+    #return BeautifulSoup(result.content, 'html.parser')
+
+    for i in range(3):
+        try:
+            # To get around it
+            # # maybe need to only launch this once and then just do requests
+            with sync_playwright() as p:
+                # Webkit is fastest to start
+                browser = p.webkit.launch(headless=True)
+                page = browser.new_page()
+                response = page.goto(url)
+                webdata = page.content()
+                    
+            return BeautifulSoup(webdata, 'html.parser')
+
+        except Exception as e:
+            print(e)
+            print("barfing on " + url)
 
 
+###### Variables for loop and tables ######
 
-#souping = get_page(base_url(4522,2))
+# itemid = 4522
+# paintid = 2
+# i_idkey = str(itemid) + "_"+ str(paintid)
+
+# souping = get_page(base_url(itemid,paintid))
 
 #testing - input manually for testing souping functions
 #this should later become part of a parent loop that gets id (249 ids per batch), then loops for each color
@@ -37,6 +60,7 @@ def get_page(url):
 #4284,0  fennec, many times in shop
 #4522,2 not in the item shop
 #4549,0  one time in the shop
+
 
 
 
@@ -59,16 +83,25 @@ def soup_name_paint(souping):
 
     item_data_js = item_data[23:-1]
 
-    item_data_dict = json.loads(item_data_js)
+    item_data_js_fix = item_data_js.replace("\\","-")
+    item_data_dict = json.loads(item_data_js_fix)
 
     return {"item name":item_data_dict["itemName"],"item paint":item_data_dict["itemColor"]}
 
 
 
 
+###### Variables for loop and tables ######
+
+# souping = get_page("https://rl.insider.gg/pc/3468/0")
 # item_basics = soup_name_paint(souping)
-# item_basics["item name"]
-# item_basics["item paint"]
+# # i_name = item_basics["item name"]
+# # i_paint =item_basics["item paint"]
+# # print(i_name)
+# # print(i_paint)
+# print(item_basics)
+
+
 
 
 
@@ -88,7 +121,7 @@ def soup_price_pc_tag(souping_page):
         return price_pc[1]
 
 
-#print(soup_price_pc_tag(souping))
+# print(soup_price_pc_tag(souping))
 
 
 
@@ -155,13 +188,16 @@ def resolve_price(clean_range):
     return price_dict
 
 #print(resolve_price((['1','0'],['1','1'])))
+
+
 #print(resolve_price(clean_price_tag(soup_price_pc_tag(souping))))
 
 
-
+###### Variables for loop and tables ######
 # price_pair = resolve_price(clean_price_tag(soup_price_pc_tag(souping)))
-# price_min = price_pair['minp']
-# price_max = price_pair['maxp']
+# i_price_min = price_pair['minp']
+# i_price_max = price_pair['maxp']
+
 
 
 
@@ -177,34 +213,37 @@ def soup_item_shop_tag(souping):
     item_shop_pc = []
     
     try:
-        for tags in souping.find(id = "itemShopContainer"):
-            item_shop_pc.append(tags.contents)
+        for each in souping.find(id = "itemShopContainer").findAll("tr"):
+            item_shop_pc.append(each.contents)
+
     except TypeError:
         return("Page not found")
 
     else:
         return item_shop_pc
 
+#print(soup_item_shop_tag(souping))
 
 
 
 def dates_in_shop(soup_item_shop):
-    """Isolating the second tag that has the Content. Returns the number of data tags in them or 0 when it has never been in the shop"""
-    tags_with_data = soup_item_shop[1]
-               
-    if tags_with_data == ['This item has not been in the Item Shop yet.']:
+    """Isolating the second tag that has the Content within a list. Returns the number of data tags in them is empty (no tr's from above) it has never been in the shop"""
+     
+    if soup_item_shop == []:
         times_in_shop_value = 0
     else:
-        times_in_shop_value = int(len(tags_with_data))
-
-
+        times_in_shop_value = int(len(soup_item_shop))
+ 
     return times_in_shop_value
 
+#print(dates_in_shop(soup_item_shop_tag(souping)))
 
 
-#num_dates_in_shop = dates_in_shop(soup_item_shop_tag(souping))
 
+###### Variables for loop and tables ######
+#i_times_in_shop = dates_in_shop(soup_item_shop_tag(souping))
 
+#print(i_times_in_shop)
 
 
 
@@ -215,37 +254,48 @@ def dates_in_shop(soup_item_shop):
 
 def recent_in_shop(soup_item_shop):
     """Only runs well if item has been in the shop. Getting the tag for the last time the item was in the in-game shop."""
-    tags_with_data = soup_item_shop[1]
-        
-    times_in_shop_v = int(len(tags_with_data))
 
+    times_in_shop_v = int(len(soup_item_shop))
     last_time_shop_i = times_in_shop_v - 1
-
-    recent_tag = tags_with_data[last_time_shop_i]
-
+    recent_tag = soup_item_shop[last_time_shop_i]
     return recent_tag
+
+#print(recent_in_shop(soup_item_shop_tag(souping)))
+
 
 
 
 def final_ingame_shop(last_time_shop_var):
     """Creating the dict with the last time in shop tag. This needs a variable that calls the recent_in_shop(). Error when item has never been in shop"""
-    date_string = last_time_shop_var.contents[len(last_time_shop_var)-3].get_text()   
+    #date_string = last_time_shop_var.contents[len(last_time_shop_var)-3].get_text()
+    date_string = last_time_shop_var[len(last_time_shop_var)-3].get_text()   
     date_value_datetime = datetime.strptime(date_string, '%b %d, %Y')
     date_value = date_value_datetime.date()
     
-    cert_tag = str(last_time_shop_var.contents[len(last_time_shop_var)-2].get_text())
+    #cert_tag = str(last_time_shop_var.contents[len(last_time_shop_var)-2].get_text())
+    cert_tag = str(last_time_shop_var[len(last_time_shop_var)-2].get_text())
     
-    price_tag = int(last_time_shop_var.contents[len(last_time_shop_var)-1].get_text())
+    #price_tag = int(last_time_shop_var.contents[len(last_time_shop_var)-1].get_text())
+    price_tag = int(last_time_shop_var[len(last_time_shop_var)-1].get_text())
 
     return {"recent date in shop":  date_value, "recent cert in shop": cert_tag, "recent price in shop": price_tag}
 
 
+#print(final_ingame_shop(recent_in_shop(soup_item_shop_tag(souping))))
 
 
-# info_last_date_shop = final_ingame_shop(recent_in_shop(soup_item_shop_tag(souping)))
-# info_last_date_shop["recent date in shop"]
-# info_last_date_shop["recent cert in shop"]
-# info_last_date_shop["recent price in shop"]
+###### Variables for loop and tables ######
+
+# if i_times_in_shop == 0:
+#     i_date_shop = "Never"
+#     i_cert_shop =  "Never"
+#     i_price_shop = 0
+
+# else:
+#     info_last_date_shop = final_ingame_shop(recent_in_shop(soup_item_shop_tag(souping)))
+#     i_date_shop = info_last_date_shop["recent date in shop"]
+#     i_cert_shop =  info_last_date_shop["recent cert in shop"]
+#     i_price_shop = info_last_date_shop["recent price in shop"]
 
 
 
@@ -260,53 +310,141 @@ def soup_item_info_tag(souping):
     item_info_pc = []
     
     try:
-        for tags in souping.find(id = "itemInfoContainer"):
-            item_info_pc.append(tags.contents)
+        item_info_pc = souping.find(id = "itemInfoContainer").findAll("tr")
+
     except TypeError:
         return("Page not found")
 
     else:
-        return item_info_pc[1]
+        return item_info_pc
 
 
 
 
 
 def item_info_content(item_info_tag_var):
-    """Getting the content in the info tag - creating dictionary for the contents. Needs soup_intem_info_tag() or the variable created for it"""
-            
+    """Getting the content in the info tag - creating dictionary for the contents. Dif order for Gift Packs, diff len of categories. Needs soup_intem_info_tag() or the variable created for it"""
+
     rarity_value = [child for child in item_info_tag_var[0]]
     type_value = [child for child in item_info_tag_var[1]]
-    series_value = [child for child in item_info_tag_var[2]]
-    release_value = [child for child in item_info_tag_var[3]]
-    paints_value = [child for child in item_info_tag_var[4]]
-    blueprint_value = [child for child in item_info_tag_var[6]]
+    
+    
+    if len(item_info_tag_var) == 6:
+        series_value = [child for child in item_info_tag_var[2]]
+        release_value = "none"
+        paints_value = [child for child in item_info_tag_var[3]]
+        blueprint_value = [child for child in item_info_tag_var[5]]
+    
+    elif len(item_info_tag_var) == 7:
+        if type_value[1].get_text() == "Gift Pack":
+            series_value = ["none"]
+            release_value = [child for child in item_info_tag_var[2]]
+
+        else:
+            series_value = [child for child in item_info_tag_var[2]]
+            release_value = [child for child in item_info_tag_var[3]]
+    
+        paints_value = [child for child in item_info_tag_var[4]]
+        blueprint_value = [child for child in item_info_tag_var[6]]
+
+    elif len(item_info_tag_var) ==8:
+        series_value = [child for child in item_info_tag_var[2]]
+        release_value = [child for child in item_info_tag_var[3]]
+        paints_value = [child for child in item_info_tag_var[5]]
+        blueprint_value = [child for child in item_info_tag_var[7]]
 
     
+
+
     s_series_value = len(series_value) - 1
     if s_series_value == 1 and len(series_value[1].get_text()) < 5 :
         s_series_value = 0
   
+    
+    if "none" in release_value:
+        s_release_value = "none"
+    else:
+        s_release_value = release_value[1].get_text()
+        
+        if "(" in s_release_value:
+            split_release_value = s_release_value.split("(")
+            s_release_value= split_release_value[0]
+    
+        s_release_value_datetime = datetime.strptime(s_release_value, '%b %d, %Y')
+        s_release_value = s_release_value_datetime.date()
 
-    s_release_value = release_value[1].get_text()
-    if "(" in s_release_value:
-        split_release_value = s_release_value.split("(")
-        s_release_value= split_release_value[0]
-    s_release_value_datetime = datetime.strptime(s_release_value, '%b %d, %Y')
-    s_release_value = s_release_value_datetime.date()
 
          
     return {"rarity":rarity_value[1].get_text(), "type":type_value[1].get_text(), "number of series":s_series_value,"release date":s_release_value,"paints available":paints_value[1].get_text(),"has blueprint":blueprint_value[1].get_text()}
 
 
 
+###### Variables for loop and tables ######
+# souping = get_page("https://rl.insider.gg/pc/2279/0")
+
+
 # info_right_col = item_info_content(soup_item_info_tag(souping))
-# info_right_col["rarity"]
-# info_right_col["type"]
-# info_right_col["number of series"]
-# info_right_col["release date"]
-# info_right_col["paints available"]
-# info_right_col["has blueprint"]
+
+# i_rarity = info_right_col["rarity"]
+# i_type = info_right_col["type"]
+# i_series = info_right_col["number of series"]
+# i_release = info_right_col["release date"]
+# i_pavail = info_right_col["paints available"]
+# i_blue = info_right_col["has blueprint"]
+
+# print(i_type)
+# print(i_rarity)
+# print(i_series)
+# print(i_release)
+# print(i_pavail)
+# print(i_blue)
+
+
+
+
+
+
+##################################
+#Connecting to SQL
+##################################
+
+
+def get_env_var():
+    '''getting credentials from env variable in the os'''
+    c1u = os.getenv("MYSQLDB_USER")
+    c2p = os.getenv("MYSQLDB_PSW")
+    c3h = os.getenv("MYSQLDB_HOST")
+    c4d = 'sys'
+    return c1u, c2p, c3h, c4d
+
+user_var = get_env_var()[0]
+psw_var = get_env_var()[1]
+host_var = get_env_var()[2]
+datab_var = get_env_var()[3]
+
+##################################
+
+
+
+# try:
+#     cnx = mysql.connector.connect(user= user_var, password= psw_var,
+#                                 host= host_var,
+#                                 database= datab_var)
+#     cursor = cnx.cursor()
+#     query = "INSERT INTO rl_items_t1(iid_key, name, paint, type, rarity, num_series, release_date, paints_avail, blueprint, price_min, price_max, num_times_in_shop, last_date_in_shop, last_cert_in_shop, last_price_in_shop)    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s)"
+#     data = [(i_idkey, i_name, i_paint, i_type, i_rarity, i_series, i_release, i_pavail, i_blue, i_price_min, i_price_max, i_times_in_shop, i_date_shop, i_cert_shop, i_price_shop)]
+#     cursor.executemany(query,data)
+#     cnx.commit()
+
+# except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with your user name or password")
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#   else:
+#     print(err)
+# else:
+#     cnx.close()
 
 
 
@@ -351,110 +489,107 @@ def resolve_batch_num(user_input_var):
 
 
 
-#iids_list = resolve_batch_num(user_input_per_batch())
+
+# ##################################
+# #Main program
+# ##################################
+
+iids_list = resolve_batch_num(user_input_per_batch())
+
+for iids in iids_list:
+    not_found_count = 0
+    print(iids)
+
+    for ipaints in range(14):
+        itemid = iids
+        paintid = ipaints
+        i_idkey = str(itemid) + "_"+ str(paintid)
+        # print(i_idkey + " before soup")
+              
+        
+        souping = get_page(base_url(itemid,paintid))
+
+
+        if soup_price_pc_tag(souping) == "Not found.":
+            not_found_count+=1
+            if not_found_count == 6:
+                # print("Not found +5 times")
+                break
+            else:
+                continue
+        else:
+            item_basics = soup_name_paint(souping)
+            i_name = item_basics["item name"]
+            i_paint =item_basics["item paint"]
+            # print(i_name)
+            # print(i_paint)
+
+            info_right_col = item_info_content(soup_item_info_tag(souping))
+            i_rarity = info_right_col["rarity"]
+            i_type = info_right_col["type"]
+            i_series = info_right_col["number of series"]
+            i_release = info_right_col["release date"]
+            i_pavail = info_right_col["paints available"]
+            i_blue = info_right_col["has blueprint"]
+            # print(i_type)        
+            # print(i_rarity)
+            # print(i_series)
+            # print(i_release)
+            # print(i_pavail)
+            # print(i_blue)
+
+            price_pair = resolve_price(clean_price_tag(soup_price_pc_tag(souping)))
+            i_price_min = price_pair['minp']
+            i_price_max = price_pair['maxp']
+            # print(i_price_min)
+            # print(i_price_max)
+
+            i_times_in_shop = dates_in_shop(soup_item_shop_tag(souping))
+            # print(i_times_in_shop)
+
+            if i_times_in_shop == 0:
+                i_date_shop = "Never"
+                i_cert_shop =  "Never"
+                i_price_shop = 0
+
+            else:
+                info_last_date_shop = final_ingame_shop(recent_in_shop(soup_item_shop_tag(souping)))
+                i_date_shop = info_last_date_shop["recent date in shop"]
+                i_cert_shop =  info_last_date_shop["recent cert in shop"]
+                i_price_shop = info_last_date_shop["recent price in shop"]
+
+            # print(i_date_shop)
+            # print(i_cert_shop)
+            # print(i_price_shop)
 
 
 
-
-##################################
-#Connecting to SQL
-##################################
-
-
-def get_env_var():
-    '''getting credentials from env variable in the os'''
-    USER = os.getenv("MYSQLDB_USER")
-    PASSWORD = os.getenv("MYSQLDB_PSW")
-    HOST = os.getenv("MYSQLDB_HOST")
-    DATABASE = 'sys'
-    return USER, PASSWORD, HOST, DATABASE
-
-user_var = get_env_var()[0]
-psw_var = get_env_var()[1]
-host_var = get_env_var()[2]
-datab_var = get_env_var()[3]
-
-
-
-#def sql_connect(env0, env1, env2, env3):
- #   '''connecting to SQL server'''
-#    cnx = mysql.connector.connect(user = env0, password = env1, host = env2, database = env3)
- #   return cnx
-
-
-#def sql_insert(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15 ):
-    #query = "INSERT INTO rl_items_t1(iid_key, name, paint, type, rarity, num_series, release_date, paints_avail, blueprint, price_max, price_min, num_days_in_shop, last_date_in_shop, last_cert_in_shop, last_price_in_shop)    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s)"
-    #data = [(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15)]
-   # return query, data
-
-
-
-col1="col1id06"
-col2="col2"
-col3="col3"
-col4="col4"
-col5="col5"
-col6= 10
-col7= "July 4, 2021"
-col8="col8"
-col9= "col9"
-col10= 1000000.5
-col11= 100000000.5
-col12= 4
-col13= "Jan 6, 2022"
-col14= "col14"
-col15= 100000
-
-
-
-
-
-try:
-    cnx = mysql.connector.connect(user= user_var, password= psw_var,
+            try:
+                cnx = mysql.connector.connect(user= user_var, password= psw_var,
                                 host= host_var,
                                 database= datab_var)
-    cursor = cnx.cursor()
-    query = "INSERT INTO rl_items_t1(iid_key, name, paint, type, rarity, num_series, release_date, paints_avail, blueprint, price_min, price_max, num_times_in_shop, last_date_in_shop, last_cert_in_shop, last_price_in_shop)    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s)"
-    data = [(col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12, col13, col14, col15)]
-    cursor.executemany(query,data)
-    cnx.commit()
-
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-    cnx.close()
-
-
+                cursor = cnx.cursor()
+                query = "INSERT INTO rl_items_t1(iid_key, name, paint, type, rarity, num_series, release_date, paints_avail, blueprint, price_min, price_max, num_times_in_shop, last_date_in_shop, last_cert_in_shop, last_price_in_shop)    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s)"
+                data = [(i_idkey, i_name, i_paint, i_type, i_rarity, i_series, i_release, i_pavail, i_blue, i_price_min, i_price_max, i_times_in_shop, i_date_shop, i_cert_shop, i_price_shop)]
+                cursor.executemany(query,data)
+                cnx.commit()
+            
+            except mysql.connector.Error as err:
+                if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                    print("Something is wrong with your user name or password")
+                elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                    print("Database does not exist")
+                else:
+                    print(err)
+            else:
+                cnx.close()
 
 
 
+        if i_pavail == "No":
+            break
 
 
-#def sql_cursor(cnx_var):
- #   cursor = cnx_var.cursor()
- #   return cursor
+        
 
-#cursor_var = sql_cursor(sql_connect(get_env_var()[0],get_env_var()[1],get_env_var()[2],get_env_var()[3]))
-
-
-
-
-
-
-
-
-
-
-
-
-#insert_in_table = sql_insert(cursor_var, "col1","col2", "col3", "col4", "col5","col6","col7", "col8", "col9", "col10", "col11","col12", "col13", "col14", "col15")
-#sql_connect(get_env_var()[0],get_env_var()[1],get_env_var()[2],get_env_var()[3]).commit()
-
-
-
-
+print("Program ended")
